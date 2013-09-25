sous-chefs/certificate

commit fb427656e4102164a9b5190bc5704c6e1842c113

certificate v0.4.0
* Add `nginx_cert` knob for chained certificates

providers/manage.rb
@@ -29,12 +29,20 @@ action :create do
     cert_directory_resource "certs"
     cert_directory_resource "private", :private => true
 
-    cert_file_resource "certs/#{new_resource.cert_file}",  ssl_item['cert']
-    cert_file_resource "certs/#{new_resource.chain_file}", ssl_item['chain']
+    if new_resource.nginx_cert
+      cert_file_resource "certs/#{new_resource.cert_file}",  "#{ssl_item['cert']}\n#{ssl_item['chain']}"
+    else
+      cert_file_resource "certs/#{new_resource.cert_file}",  ssl_item['cert']
+      cert_file_resource "certs/#{new_resource.chain_file}", ssl_item['chain']
+    end
     cert_file_resource "private/#{new_resource.key_file}", ssl_item['key'], :private => true
   else
-    cert_file_resource new_resource.cert_file,  ssl_item['cert']
-    cert_file_resource new_resource.chain_file, ssl_item['chain']
+    if new_resource.nginx_cert
+      cert_file_resource "certs/#{new_resource.cert_file}",  "#{ssl_item['cert']}\n#{ssl_item['chain']}"
+    else
+      cert_file_resource "certs/#{new_resource.cert_file}",  ssl_item['cert']
+      cert_file_resource "certs/#{new_resource.chain_file}", ssl_item['chain']
+    end
     cert_file_resource new_resource.key_file,   ssl_item['key'], :private => true
   end
 end

resources/manage.rb
@@ -46,6 +46,7 @@ attribute :cert_path, :kind_of => String, :default => "/opt/local/etc/openssl"
 else
 attribute :cert_path, :kind_of => String, :default => "/etc/ssl"
 end
+attribute :nginx_cert, :kind_of => [ TrueClass, FalseClass ], :default => false
 attribute :cert_file, :kind_of => String, :default => "#{node['fqdn']}.pem"
 attribute :key_file, :kind_of => String, :default => "#{node['fqdn']}.key"
 attribute :chain_file, :kind_of => String, :default => "#{node['hostname']}-bundle.crt"

.kitchen.yml
@@ -1,25 +1,13 @@
 ---
 driver_plugin: vagrant
 driver_config:
-  require_chef_omnibus: true
+  require_chef_omnibus: latest 
 
 platforms:
 - name: ubuntu-12.04
-  driver_config:
-    box: opscode-ubuntu-12.04
-    box_url: https://opscode-vm.s3.amazonaws.com/vagrant/opscode_ubuntu-12.04_provisionerless.box
 - name: ubuntu-10.04
-  driver_config:
-    box: opscode-ubuntu-10.04
-    box_url: https://opscode-vm.s3.amazonaws.com/vagrant/opscode_ubuntu-10.04_provisionerless.box
 - name: centos-6.4
-  driver_config:
-    box: opscode-centos-6.4
-    box_url: https://opscode-vm.s3.amazonaws.com/vagrant/opscode_centos-6.4_provisionerless.box
 - name: centos-5.9
-  driver_config:
-    box: opscode-centos-5.9
-    box_url: https://opscode-vm.s3.amazonaws.com/vagrant/opscode_centos-5.9_provisionerless.box
 
 suites:
 - name: default
@@ -29,6 +17,7 @@ suites:
   attributes:
     certificate: [
       test: {
+        nginx_cert: true,
         cert_file: "test.pem",
         key_file: "test.key"
       }

Gemfile
@@ -1,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'berkshelf'
+gem 'knife-solo_data_bag'
+gem 'test-kitchen', git: 'git://github.com/opscode/test-kitchen.git', branch: 'master'
+gem 'kitchen-vagrant', git: 'git://github.com/adesso-mobile/kitchen-vagrant.git', branch: 'master', :group => 'integration'

commit b919fc7aab9a584ec07b651d1f0756d10b621a6e

fb42765 broke handling of create_subdirs False

providers/manage.rb
@@ -38,12 +38,12 @@ action :create do
     cert_file_resource "private/#{new_resource.key_file}", ssl_item['key'], :private => true
   else
     if new_resource.nginx_cert
-      cert_file_resource "certs/#{new_resource.cert_file}",  "#{ssl_item['cert']}\n#{ssl_item['chain']}"
+      cert_file_resource new_resource.cert_file,  "#{ssl_item['cert']}\n#{ssl_item['chain']}"
     else
-      cert_file_resource "certs/#{new_resource.cert_file}",  ssl_item['cert']
-      cert_file_resource "certs/#{new_resource.chain_file}", ssl_item['chain']
+      cert_file_resource new_resource.cert_file,  ssl_item['cert']
+      cert_file_resource new_resource.chain_file, ssl_item['chain']
     end
-    cert_file_resource new_resource.key_file,   ssl_item['key'], :private => true
+    cert_file_resource new_resource.key_file, ssl_item['key'], :private => true
   end
 end
 
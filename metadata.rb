294640a9c998cc7e72291938df17bcb230a58a16
Christian Höltje <docwhat@gerf.org>
2019-03-01 10:19:04 -0500

    chore(metadata): add source and issues url

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,9 @@ license 'Apache 2.0'
 description 'Installs/Configures certificates, private keys, CA root bundles from encrypted data bags.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
+source_url 'https://github.com/mburns/cookbook-certificate'
+issues_url 'https://github.com/mburns/cookbook-certificate/issues'
+
 %w( amazon centos debian fedora redhat oracle scientific ubuntu smartos ).each do |os|
   supports os
 end

ef42a55963cf6a093c55d3f83b13180bfd1d4498
Christian Höltje <docwhat@gerf.org>
2019-04-08 13:23:56 -0400

    chore(metadata): add issues and source urls
    This will improve the data in the supermarket.

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,12 @@ license 'Apache 2.0'
 description 'Installs/Configures certificates, private keys, CA root bundles from encrypted data bags.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
+source_url 'https://github.com/mburns/cookbook-certificate' if respond_to?(:source_url)
+issues_url 'https://github.com/mburns/cookbook-certificate/issues' if respond_to?(:issues_url)
 %w( amazon centos debian fedora redhat oracle scientific ubuntu smartos ).each do |os|
   supports os
 end
+
+
 
 depends 'chef-vault'

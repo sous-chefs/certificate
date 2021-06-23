356876c02d504dffa649038b006fe55bb1368096
Robert Detjens <detjensrobert@osuosl.org>
2021-06-23 16:05:04 -0700

    Update metadata to Sous Chefs config
    Signed-off-by: Robert Detjens <detjensrobert@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -13,3 +13,28 @@ end
 end
 
 depends 'chef-vault'
+
+name             'certificate'
+source_url       'https://github.com/sous-chefs/certificate'
+issues_url       'https://github.com/sous-chefs/certificate/issues'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
+chef_version     '>= 15.3'
+license          'Apache-2.0'
+description      'Installs and configures certificates, private keys, CA root bundles from encrypted data bags.'
+version          '1.0.0'
+
+supports 'amazon'
+supports 'arch'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'freebsd'
+supports 'opensuse'
+supports 'opensuseleap'
+supports 'redhat'
+supports 'scientific'
+supports 'suse'
+supports 'ubuntu'
+
+depends 'chef-vault'
